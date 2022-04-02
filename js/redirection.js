fa3b038388aeb685b98f25829f07a4fb62f5df71
wassim <wassimbouzakri@icloud.com>
2022-04-02 15:05:29 +0200

    :sparkles: #3 Add loadMore to productPage

--- js/redirection.js
+++ js/redirection.js
@@ -1,5 +1,5 @@
 if (localStorage.getItem("token") === null || document.cookie.indexOf("authToken") === -1) {
-  window.location.href = "../pages/login/index.html";
+  window.location.href = "/pages/login/index.html";
 } else {
-  window.location.href = "../pages/products/index.html";
+  window.location.href = "/pages/products/index.html";
 }
