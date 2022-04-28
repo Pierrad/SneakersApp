dde51f4d6ff3b3916cae8ac1f4430bdb92812c24
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-04-28 10:55:40 +0200

    🚀 add location

---
+++ pages/products/map.js
@@ -0,0 +1,22 @@
+var map = L.map('map').setView([51.505, -0.09], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+getProducts();
+let items = JSON.parse(localStorage.getItem('products'));
+
+for (let i = 0; i < items.length; i++) {
+    L.marker([parseFloat(items[i].x), parseFloat(items[i].y)]).addTo(map)
+        .bindPopup(items[i].name)
+        .openPopup();
+}
